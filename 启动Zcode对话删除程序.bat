2ddbbcc80e27efb2706a3a6e5a595ab22fe6bb5c
uahz <uahz@qq.com>
2026-08-29 18:16:22 +0800

    add 启动Zcode对话删除程序.bat

---
+++ 启动Zcode对话删除程序.bat
@@ -0,0 +1,4 @@
+@echo off
+chcp 65001 >nul
+title Zcode 对话删除程序
+"C:\Users\Administrator\.workbuddy\binaries\python\envs\default\Scripts\pythonw.exe" "C:\Users\Administrator\WorkBuddy\12\zcode-cleaner\main.py"
